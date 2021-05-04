afdfe68180117548fafabdfbcd3f26a4e6d1b17f
Mark Steffensen <mark.steffensen@microfocus.com>
2021-05-04 15:28:07 +0000

    Initial commit

---
+++ Login_Logout_Web/data/action.c
@@ -0,0 +1,294 @@
+Action()
+{
+
+	web_set_user("Mercury", 
+		lr_unmask("60907177e7c411aa6aea54"), 
+		"nimbusserver.aos.com:8002");
+
+	web_add_cookie("userCart=%7B%22userId%22%3A-1%2C%22productsInCart%22%3A%5B%5D%7D; DOMAIN=nimbusserver.aos.com");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("nimbusserver.aos.com:8000", 
+		"URL=http://nimbusserver.aos.com:8000/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/main.min.js", ENDITEM, 
+		"Url=/services.properties", ENDITEM, 
+		"Url=/css/images/logo.png", ENDITEM, 
+		"Url=/css/images/closeDark.png", ENDITEM, 
+		"Url=/css/fonts/roboto_regular_macroman/Roboto-Regular-webfont.woff", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_medium_macroman/Roboto-Medium-webfont.woff", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_light_macroman/Roboto-Light-webfont.woff", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/images/arrow_right.png", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=mice", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=headphones", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=laptops", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=tablets", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=speakers", ENDITEM, 
+		"Url=/css/images/Special-offer.jpg", ENDITEM, 
+		"Url=/css/fonts/roboto_bold_macroman/Roboto-Bold-webfont.woff", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_thin_macroman/Roboto-Thin-webfont.woff", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/images/GoUp.png", ENDITEM, 
+		"Url=/css/images/facebook.png", ENDITEM, 
+		"Url=/css/images/twitter.png", ENDITEM, 
+		"Url=/css/images/linkedin.png", ENDITEM, 
+		"Url=/css/images/Banner3.jpg", ENDITEM, 
+		"Url=/css/images/Banner2.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item3.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item2.jpg", ENDITEM, 
+		"Url=/css/images/Banner1.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item1.jpg", ENDITEM, 
+		"Url=/css/images/FacebookLogo.png", "Referer=http://nimbusserver.aos.com:8000/css/main.min.css", ENDITEM, 
+		"Url=/css/images/category_banner_4.png", ENDITEM, 
+		"Url=/css/images/Filter.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4700", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4600", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4400", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4500", ENDITEM, 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Origin", 
+		"http://nimbusserver.aos.com:8000");
+
+	web_url("ALL", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/DemoAppConfig/parameters/by_tool/ALL", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("Access-Control-Request-Headers", 
+		"content-type,soapaction");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"cors");
+
+	lr_think_time(9);
+
+	web_custom_request("GetAccountConfigurationRequest", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("SOAPAction", 
+		"com.advantage.online.store.accountserviceGetAccountConfigurationRequest");
+
+	web_custom_request("GetAccountConfigurationRequest_2", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><GetAccountConfigurationRequest xmlns=\"com.advantage.online.store.accountservice\"></GetAccountConfigurationRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	web_url("categories", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("search", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/deals/search?dealOfTheDay=true", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_cookie("_ga=GA1.2.1595792217.1620078846; DOMAIN=nimbusserver.aos.com");
+
+	web_add_cookie("_gid=GA1.2.462067801.1620078846; DOMAIN=nimbusserver.aos.com");
+
+	web_add_cookie("_gat=1; DOMAIN=nimbusserver.aos.com");
+
+	web_revert_auto_header("Origin");
+
+	web_url("popularProducts.json", 
+		"URL=http://nimbusserver.aos.com:8000/app/tempFiles/popularProducts.json", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("home-page.html", 
+		"URL=http://nimbusserver.aos.com:8000/app/views/home-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_start_transaction("AOS_Home");
+
+	lr_end_transaction("AOS_Home",LR_AUTO);
+
+	lr_start_transaction("AOS_Profile");
+
+	web_add_header("Access-Control-Request-Headers", 
+		"content-type,soapaction");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	web_add_auto_header("Origin", 
+		"http://nimbusserver.aos.com:8000");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"cors");
+
+	lr_think_time(10);
+
+	web_custom_request("AccountLoginRequest", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/AccountLoginRequest", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("SOAPAction", 
+		"com.advantage.online.store.accountserviceAccountLoginRequest");
+
+	web_custom_request("AccountLoginRequest_2", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/AccountLoginRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><AccountLoginRequest xmlns=\"com.advantage.online.store.accountservice\"><email></email><loginPassword>Mercury</loginPassword><loginUser>Mercury</loginUser></AccountLoginRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	web_add_header("Access-Control-Request-Headers", 
+		"authorization");
+
+	web_add_header("Access-Control-Request-Method", 
+		"GET");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"cors");
+
+	web_custom_request("675949597", 
+		"URL=http://nimbusserver.aos.com:8002/order/api/v1/carts/675949597", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t11.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_set_sockets_option("INITIAL_AUTH", "BASIC");
+
+	web_url("675949597_2", 
+		"URL=http://nimbusserver.aos.com:8002/order/api/v1/carts/675949597", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t12.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("Access-Control-Request-Headers", 
+		"content-type,soapaction");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	web_add_header("Sec-Fetch-Mode", 
+		"cors");
+
+	lr_think_time(7);
+
+	web_custom_request("AccountLogoutRequest", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/AccountLogoutRequest", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t13.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("SOAPAction", 
+		"com.advantage.online.store.accountserviceAccountLogoutRequest");
+
+	web_custom_request("AccountLogoutRequest_2", 
+		"URL=http://nimbusserver.aos.com:8001//accountservice/ws/AccountLogoutRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t14.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><AccountLogoutRequest xmlns=\"com.advantage.online.store.accountservice\"><loginUser>675949597</loginUser><base64Token>Basic TWVyY3VyeTpNZXJjdXJ5</base64Token></AccountLogoutRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	lr_end_transaction("AOS_Profile",LR_AUTO);
+
+	lr_think_time(25);
+
+	web_url("products", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories/4/products", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t15.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("attributes", 
+		"URL=http://nimbusserver.aos.com:8002/catalog/api/v1/categories/attributes", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t16.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_revert_auto_header("Origin");
+
+	web_url("category-page.html", 
+		"URL=http://nimbusserver.aos.com:8000/app/views/category-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://nimbusserver.aos.com:8000/", 
+		"Snapshot=t17.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
